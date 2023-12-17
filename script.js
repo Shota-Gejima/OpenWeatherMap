1fb9d737b8f59bbc05c1ced44c03d406261e4270
EC2 Default User <143868019+Shota-Gejima@users.noreply.github.com>
2023-12-17 07:20:41 +0000

    finish

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
+const API_KEY = "b24c884bb7b3acd8666bea8edfad7b41";
 
-$('#tab-menu a').on('click', function(e){
-	$('#tab-contents .tab').hide();
-	$('#tab-menu .active').removeClass("active");
-	$(this).addClass("active");
-	$($(this).attr("href")).show();
-	e.preventDefault();
+$(function(){
+	$('#btn').on('click', function(){
+		$.ajax({
+			url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+			dateType :'jsonp',
+		}).done(function (date){
+			$('#place').text(date.name);
+			$('#temp_max').text(date.main.temp_max);
+			$('#temp_min').text(date.main.temp_min);
+			$('#humidity').text(date.main.humidity);
+			$('#speed').text(date.wind.speed);
+			$('#weather').text(date.weather[0].main);
+			$('img').attr("src", "http://openweathermap.org/img/w/" + date.weather[0].icon + ".png");
+			$('img').attr("alt",date.weather[0].main);
+		}).fail(function (date){
+			alert('通信に失敗しました。');
+		});
+	});
 });
